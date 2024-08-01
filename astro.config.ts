ae606e879ca7f75d3e52e9e5a53e33d9b608bb1c
SNRainiar <rainiar@foxmail.com>
2024-08-01 09:06:19 +0800

    feat: add markdown plugins

--- astro.config.ts
+++ astro.config.ts
@@ -6,10 +6,31 @@ import { resolve } from 'path';
 import icon from 'astro-icon';
 import tailwind from '@astrojs/tailwind';
 
+// Remark plugins
+import {
+  remarkExtendedTable,
+  extendedTableHandlers
+} from 'remark-extended-table';
+
+// Rehype plugins
+import rehypeExternalLinks from 'rehype-external-links';
+
 // Export config
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS ? '/carbon' : undefined,
   integrations: [tailwind(), icon({ include: { mdi: ['*'] } })],
+  markdown: {
+    remarkPlugins: [remarkExtendedTable],
+    remarkRehype: {
+      handlers: { ...extendedTableHandlers }
+    },
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        { rel: ['noopener', 'noreferer'], target: '_blank' }
+      ]
+    ]
+  },
   site: process.env.GITHUB_ACTIONS ? 'https://samunatsu.github.io' : undefined,
   vite: {
     resolve: {
